c837044c1f0f2acae1a174e1d1e903aca211b187
saint miracle <nanagyeman79@gmail.com>
2025-08-28 10:40:12 +0000

    v1.0.3: Injected brand favicon via Head in _app.js

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -4,7 +4,7 @@ import { AuthProvider } from "../src/contexts/AuthContext";
 import { ReferralProvider } from "../src/contexts/ReferralContext";
 import NavBar from "../src/components/NavBar";
 import AnnouncementBar from "../src/components/AnnouncementBar";
-
+<link rel="icon" href="/aura-harvest-favicon.ico" type="image/x-icon" />
 export default function MyApp({ Component, pageProps }) {
   return (
     <AuthProvider>
